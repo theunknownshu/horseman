516bc4f8d0b6c38263792163c47b7a878ff3a066
OliveBounty <oliveodobounty@gmail.com>
2021-12-07 23:43:36 +0900

    feat: intro section precomplete

--- src/components/introduce/Introduce.js
+++ src/components/introduce/Introduce.js
@@ -1,16 +1,42 @@
 import "./Introduce.css";
 import img_intro from "../../assets/images/img_intro.jpg";
-import { Typography,Button } from "@mui/material";
+import { Typography, Button } from "@mui/material";
 import { introduce_json } from "../../constant/_helper";
 
 function Introduce() {
   return (
     <div className="container">
       <img className="img-fluid" src={img_intro} />
-      <Typography variant="h3" sx = {{fontSize:['1.5rem','2rem','3rem','3rem']}}>{introduce_json.title}</Typography>
-      <Typography sx = {{fontSize:['10px','12px','16px','20px']}}>{introduce_json.content}</Typography>
+      <Typography
+        variant="h3"
+        sx={{ fontSize: ["1.5rem", "2rem", "3rem", "3rem"] }}
+      >
+        {introduce_json.title}
+      </Typography>
+      <Typography
+        sx={{ fontSize: ["10px", "12px", "16px", "20px"] }}
+        // dangerouslySetInnerHTML={{ __html: '<p>introduce_json.<br>content</p>' }}
+      >
+        {introduce_json.content1}
+      </Typography>
+      <br></br>
+      <Typography
+        sx={{ fontSize: ["10px", "12px", "16px", "20px"] }}
+        // dangerouslySetInnerHTML={{ __html: '<p>introduce_json.<br>content</p>' }}
+      >
+        {introduce_json.content2}
+      </Typography>
+      <br></br>
+      <Typography
+        sx={{ fontSize: ["10px", "12px", "16px", "20px"] }}
+        // dangerouslySetInnerHTML={{ __html: '<p>introduce_json.<br>content</p>' }}
+      >
+        {introduce_json.content3}
+      </Typography>
 
-      <Button variant="contained" sx={{ borderRadius: "20px", px:5 }}>White Paper</Button>
+      <Button variant="contained" sx={{ borderRadius: "20px", px: 5 }}>
+        White Paper
+      </Button>
       <br></br>
       <br></br>
       <div>
--- src/constant/_helper.js
+++ src/constant/_helper.js
@@ -1,11 +1,27 @@
 export const roadmap_json = [
   {
-    percent: "0%",
-    text: "The Horseman Club is public.Roadmap & Web are open and we are live!"
+    percent: "",
+    text: "Horseman club community is open. Join us on Discord to enter in the whitelist and discover the reveal of pre-sale and public sale."
   },
   {
-    percent: "10%",
-    text: "information"
+    percent: "25%",
+    text: "When 25% is reached we will make the first Giveaway for HORSERS. An airderop of 10 NFTS wills be given."
+  },
+  {
+    percent: "50%",
+    text: "When 50% of the Horseman is reached we'll be donating $20,000 to a charity chosen by the community"
+  },
+  {
+    percent: "70%",
+    text: "When 75% of the Horsemen sell out, we'll start setting up a member exclusive merchandize store and make a giveawya for HORSERS"
+  },
+  {
+    percent: "100%",
+    text: "When 100% of the Horsemen sell out, we'll open the Private Discord for Holders. Special Events and Giveaways will be published monthly. Just for Hlders!"
+  },
+  {
+    percent: "",
+    text: "The commuinity is the most important part for us and every member is unique. Our secondary core aspect is to Join Metaverse with the identity and values that represent us. The Horsers will be whitelisted in the 3D Avatars to Join Metaverse as Horseman Club members."
   }
 ];
 export const faq_json = [
@@ -37,8 +53,11 @@ export const faq_json = [
 ];
 export const introduce_json = {
   title: "10,000 unique and indomable Horsemen",
-  content:
-    "In the depths of metaverse there's a community of winning horsemen. In a fast paced and transitioned world just the fastest ones will be the first ones on reaching power in the future.The Horseman Club is a distintive collection of 10,000 art pieces that lie in the ERC-721. Each one of this tokens* is unique and indomable and rules his own life. This are thoughtfully designed, specifically picked, and impeccably shaped and there are more than 200 distinctive creatives. We are e4xcited for future and the community is the core aspect of Horseman Club. We want that every holder has the power and is representative of Horseman with access on giveaways, events and a lot more. Join us on our adventure and our club and let's rule the metaverse."
+  content1:
+    "In the depths of metaverse there's a community of winning horsemen. In a fast paced and transitioned world just the fastest ones will be the first ones on reaching power in the future."
+    ,
+    content2: "The Horseman Club is a distintive collection of 10,000 art pieces that lie in the ERC-721. Each one of this tokens* is unique and indomable and rules his own life. This are thoughtfully designed, specifically picked, and impeccably shaped and there are more than 200 distinctive creatives.",
+    content3:" We are e4xcited for future and the community is the core aspect of Horseman Club. We want that every holder has the power and is representative of Horseman with access on giveaways, events and a lot more. Join us on our adventure and our club and let's rule the metaverse."
 };
 
 // export const nftlist_img_base = "";
