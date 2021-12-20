33cbef019eb60e3fab2cfbb9f208914066ca74ae
OliveBounty <oliveodobounty@gmail.com>
2021-12-20 11:17:08 +0900

    feat: team section style define

--- src/components/team/Team.js
+++ src/components/team/Team.js
@@ -1,18 +1,12 @@
 import { Typography } from "@mui/material";
 import { team_json } from "../../constant/_helper";
 import "./Team.css";
-// import developer from "../../assets/images/team/developer.JPG";
+import { global_styles } from "../../constant/_helper";
+
 function Team() {
   return (
     <div id="id_team" className="container pt-5">
-      <Typography
-        variant="h3"
-        sx={{
-          fontSize: ["1.5rem", "2rem", "3rem", "3rem"],
-          fontFamily: `"Montserrat", Sans-serif`,
-          fontWeight: "800"
-        }}
-      >
+      <Typography variant="h3" sx={global_styles.headerstyle}>
         TEAM
       </Typography>
       {/* <div className=" container mt-5 row" style = {{margin:0}}>
@@ -39,7 +33,10 @@ function Team() {
       </div> */}
       <div className="row mt-2">
         <div className="col-md-6 d-flex">
-          <div className = " d-flex justify-center" style = {{flexDirection:"column", justifyContent:"center"}}>
+          <div
+            className=" d-flex justify-center"
+            style={{ flexDirection: "column", justifyContent: "center" }}
+          >
             <Typography sx={{ fontFamily: `"Poppins", Sans-serif` }}>
               The Horseman Club has been created by a group of four friends that
               wanted to join and share their art and love for the blockchain
